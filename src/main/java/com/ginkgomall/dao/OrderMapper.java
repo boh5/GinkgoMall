299bde97c9242cd3de8f7c2ac0beb6cc97182961
dill <bohuang1994@gmail.com>
2018-01-25 23:07:20 +0800

    2018/1/25 night; init project day1

---
+++ src/main/java/com/ginkgomall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.ginkgomall.dao;
+
+import com.ginkgomall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/main/java/com/ginkgomall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.ginkgomall.dao;
+
+import com.ginkgomall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
